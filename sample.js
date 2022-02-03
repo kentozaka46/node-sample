6ff6db9269dc9e9443ee3010f3420fc784e02950
kentozaka46 <kentoball52@gmail.com>
2022-02-04 07:44:44 +0900

    UI作成

--- sample.js
+++ sample.js
@@ -1,47 +1,43 @@
 /* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
-const express = require("express");
-const app = express();
-
+var express = require("express");
+var app = express();
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-const server = app.listen(3000, function(){
+var server = app.listen(3000, function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
-
 /* 3. 以後、アプリケーション固有の処理 */
-
 // 写真のサンプルデータ
-const photoList = [
+var photoList = [
     {
         id: "001",
         name: "photo001.jpg",
         type: "jpg",
         dataUrl: "http://localhost:3000/data/photo001.jpg"
-    },{
+    }, {
         id: "002",
         name: "photo002.jpg",
         type: "jpg",
         dataUrl: "http://localhost:3000/data/photo002.jpg"
     }
-]
-
+];
 // 写真リストを取得するAPI
-app.get("/api/photo/list", function(req, res, next){
+app.get("/api/photo/list", function (req, res, next) {
     res.json(photoList);
 });
-
 // 写真リストを取得するAPI
-app.get("/api/photo/list", function(req, res){
-    let photo: {
-        id: string;
-        name: string;
-        type: string;
-        dataUrl: string;
-    };
-    let i: number;
-    for(i=0; i<photoList.length; i++) {
-        if (photoList[i].id == req.params.photoId){
+app.get("/api/photo/list", function (req, res) {
+    var photo;
+    var i;
+    for (i = 0; i < photoList.length; i++) {
+        if (photoList[i].id == req.params.photoId) {
             photo = photoList[i];
         }
     }
     res.json(photo);
+});
+// View EngineにEJSを指定。
+app.set('view engine', 'ejs');
+// "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
+app.get("/", function (req, res, next) {
+    res.render("index.ejs", {});
 });
---
+++ sample.ts
@@ -0,0 +1,55 @@
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+const express = require("express");
+const app = express();
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+const server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+/* 3. 以後、アプリケーション固有の処理 */
+
+// 写真のサンプルデータ
+const photoList = [
+    {
+        id: "001",
+        name: "photo001.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo001.jpg"
+    },{
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo002.jpg"
+    }
+]
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res, next){
+    res.json(photoList);
+});
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res){
+    let photo: {
+        id: string;
+        name: string;
+        type: string;
+        dataUrl: string;
+    };
+    let i: number;
+    for(i=0; i<photoList.length; i++) {
+        if (photoList[i].id == req.params.photoId){
+            photo = photoList[i];
+        }
+    }
+    res.json(photo);
+});
+
+// View EngineにEJSを指定。
+app.set('view engine', 'ejs');
+
+// "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
+app.get("/", function(req, res, next){
+    res.render("index.ejs", {});
+});
