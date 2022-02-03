358fa74d2c8ff90005b3c18491674cd2edd51218
kentozaka46 <kentoball52@gmail.com>
2022-02-03 21:28:44 +0900

    API作成

--- sample.ts
+++ sample.ts
@@ -1,5 +1,30 @@
-const user = (name) => {
-    return 'こんにちは、' + name + 'さん';
-}
- 
-console.log(user('太郎'));
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+const express = require("express");
+const app = express();
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+const server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+/* 3. 以後、アプリケーション固有の処理 */
+
+// 写真のサンプルデータ
+const photoList = [
+    {
+        id: "001",
+        name: "photo001.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo001.jpg"
+    },{
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo002.jpg"
+    }
+]
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res, next){
+    res.json(photoList);
+});
